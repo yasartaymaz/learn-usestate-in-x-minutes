26efee42e383810a60cb1f4fec02c6574c9ae3a5
yasartaymaz <yasartaymaz@windowslive.com>
2023-06-15 17:39:05 +0300

    no message

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount(count - 1);
+  }
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  );
+}
+
+export default App;
